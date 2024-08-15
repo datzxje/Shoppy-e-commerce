9db2ce70a8c69c1a45494bbf57ed79acec99b545
datzxje <tudabest12@gmail.com>
2024-08-15 11:46:00 +0700

    First Commit

---
+++ ShoppyProject/ShoppyWebParent/ShoppyBackEnd/src/main/java/com/shoppy/admin/ShoppyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shoppy.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppyBackEndApplication.class, args);
+	}
+
+}
